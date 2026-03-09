36212b274d3caef620a622b3febcd05283e8312b
mint.nguyen <nguyenminh2707@gmail.com>
2026-03-09 11:58:14 +0700

    feat: done ex1

---
+++ tests/lesson-03/ex1.js
@@ -0,0 +1,14 @@
+const inputNumber = 9;
+
+//print out the result
+if (inputNumber === 0) {
+    console.log("This is zero");
+}
+else if
+    (inputNumber > 0) {
+    console.log("This is a positive number")
+}
+else {
+    console.log("This is a negative number")
+}
+
